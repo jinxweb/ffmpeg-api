33fcaa12fd5dd8f13e9848a1b721e6038fa7650b
Ron Jenkins <ronjinx@gmail.com>
2024-02-12 08:52:26 -0500

    #3 Closed Done - Swagger page will now display on release builds

--- ffmpeg-api/Program.cs
+++ ffmpeg-api/Program.cs
@@ -1,3 +1,6 @@
+using Ffmpeg.Library.Abstract;
+using Ffmpeg.Library.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,14 +10,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<ISubtitlesService, SubtitlesService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
